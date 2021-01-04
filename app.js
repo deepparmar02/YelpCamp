1fcfb800c1e2323c4f570ec9ea3c5350a5d24dba
deepparmar02 <deep52p@gmail.com>
2021-01-03 22:03:21 -0500

    added the U and D of CRUD

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const methodOverride = require('method-override');
+
 const mongoose = require('mongoose');
 const Campground = require('./models/campgrounds')
 
@@ -19,6 +21,9 @@ db.once("open", () => {
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
+app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride('_method'));
+
 app.get('/', (req, res) => {
     res.render('home');
 })
@@ -28,11 +33,40 @@ app.get('/campground', async (req, res) => {
     res.render('campgrounds/index', { camps });
 })
 
+app.get('/campground/new', (req, res) => {
+    res.render('campgrounds/new')
+})
+
 app.get('/campground/:id', async (req, res) => {
     const { id } = req.params;
     const campground = await Campground.findById(id);
     res.render('campgrounds/show', { campground });
 })
+
+app.get('/campground/:id/edit', async (req, res) => {
+    const { id } = req.params;
+    const campground = await Campground.findById(id);
+    res.render('campgrounds/edit', { campground });
+})
+
+app.post('/campground', async (req, res) => {
+    const campground = new Campground(req.body.campground);
+    await campground.save();
+    res.redirect(`/campground/${campground._id}`);
+})
+
+app.put('/campground/:id', async (req, res) => {
+    const { id } = req.params;
+    const campground = await Campground.findByIdAndUpdate(id, { ...req.body.campground });
+    res.redirect(`/campground/${campground._id}`);
+})
+
+app.delete('/campground/:id', async (req, res) => {
+    const { id } = req.params;
+    await Campground.findByIdAndDelete(id);
+    res.redirect('/campground');
+})
+
 
 app.listen(3000, () => {
     console.log('listening 3000');
